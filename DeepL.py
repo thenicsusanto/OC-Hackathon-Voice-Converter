b2de08876d40b1de88a023f9aed5cdfcccc83684
Aayush <66886829+aayush-exe@users.noreply.github.com>
2023-05-07 11:51:34 -0700

    Update DeepL.py
    remove

--- DeepL.py
+++ DeepL.py
@@ -2,7 +2,7 @@ import deepl
 
 def translate(text, target):
     print("Translating: " + text + " in " + target)
-    translator = deepl.Translator('e9cbd685-6cf3-e907-9336-b0a06e3def40:fx') 
+    translator = deepl.Translator("ENTER KEY HERE") 
     result = translator.translate_text(text, target_lang=target) 
     translated_text = result.text
     print("Result: " + translated_text)
