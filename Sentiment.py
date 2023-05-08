6f8ac7a077e9c00d14ece80aa17ce26f3def0d5d
Siho Choi <78015683+SihoChoii@users.noreply.github.com>
2023-05-08 11:41:31 -0700

    Update Sentiment.py
    removed key 😓

--- Sentiment.py
+++ Sentiment.py
@@ -5,7 +5,7 @@ url = "https://twinword-sentiment-analysis.p.rapidapi.com/analyze/"
 querystring = {"text":"great value in its price range!"}
 
 headers = {
-	"X-RapidAPI-Key": "07b1d0f1efmsh96130b6ca0a3fbdp17946cjsn9c1f9a89c7f2",
+	"X-RapidAPI-Key": "KEY",
 	"X-RapidAPI-Host": "twinword-sentiment-analysis.p.rapidapi.com"
 }
 
